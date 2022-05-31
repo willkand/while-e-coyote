732efa27ef25b46fe29da6c80ae2bd8c510f8043
Andre K <andrek@Andres-MacBook-Pro.local>
2022-05-31 14:53:58 -0400

    1st commit

---
+++ number-summer.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+let input = (prompt("Enter a number or type 'done': "))
+let sum = 0;
+
+while (input !== 'done') {
+    sum = sum + Number(input);
+    input = prompt("Enter another number or type 'done': ")
+}
+
+console.log("Sum:", sum);
