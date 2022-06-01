d8fb0a21b92857838fb6d8c45da88c4df6805fce
Andre K <andrek@Andres-MacBook-Pro.local>
2022-06-01 11:02:38 -0400

    2nd commit

--- number-doubler.js
+++ number-doubler.js
@@ -3,7 +3,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 let input = Number(prompt("Pick a number any number: "));
 let sum = input;
 
-while (sum <=100){
+while (sum < 100){
     sum = sum * 2
     console.log(sum);
 }
